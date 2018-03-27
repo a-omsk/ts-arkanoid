a6aa1c000876addae07f20734ac6120fe2a13dcd
Artur Shelashskiy <ashelshsky@yandex-team.ru>
2018-03-27 20:42:22 +0300

    Vaus rendering and moving implemented

--- src/Game.ts
+++ src/Game.ts
@@ -1,18 +1,34 @@
 import Brick from './Brick';
+import Vaus from './Vaus';
 
 export default class Game {
     public score: number;
     private ctx: CanvasRenderingContext2D;
     private bricks: Brick[];
+    private vaus: Vaus | null;
 
     public constructor(ctx: CanvasRenderingContext2D) {
         this.ctx = ctx;
 
         this.bricks = [];
         this.score = 0;
+
+        this.vaus = null;
     }
 
-    private buildBricks() {
+    private buildVaus(): void {
+        const vausWidth = 120;
+        const vausHeight = 30;
+
+        const vausX = Math.floor(this.ctx.canvas.clientWidth / 2) - (vausWidth / 2);
+        const vausY = this.ctx.canvas.clientHeight - vausHeight - 10;
+
+        this.vaus = new Vaus(vausX, vausY, vausWidth, vausHeight, 'green');
+
+        this.vaus.draw(this.ctx);
+    }
+
+    private buildBricks(): void {
         let currentColor = 'blue';
         const initialColumnPosition = 70;
 
@@ -39,8 +55,29 @@ export default class Game {
         }
     }
 
+    private bindVausControl() {
+        const { canvas } = this.ctx;
+        const {left} = canvas.getBoundingClientRect();
+
+        canvas.addEventListener('mousemove', e => {
+            const newVausPosition = e.clientX - left;
+
+            if (this.vaus) {
+                this.vaus.move(newVausPosition, canvas.width);
+
+                // todo: delete this
+                this.vaus.draw(this.ctx);
+            }
+        }, {passive: true})
+
+    }
+
     public start(): void {
         this.score = 0;
+
         this.buildBricks();
+        this.buildVaus();
+
+        this.bindVausControl();
     }
 }
--- src/Vaus.ts
+++ src/Vaus.ts
@@ -1,3 +1,56 @@
 export default class Vaus {
-    
+    public x: number;
+    public y: number;
+
+    public width: number;
+    public height: number;
+
+    public color: string;
+
+    public newX: number | null;
+
+    constructor(x: number, y: number, width: number, height: number, color: string) {
+        this.x = x;
+        this.y = y;
+
+        this.newX = null;
+
+        this.width = width;
+        this.height = height;
+
+        this.color = color;
+    }
+
+    draw(ctx: CanvasRenderingContext2D) {
+        if (this.newX !== null && this.newX !== this.x) {
+            this.clear(ctx);
+
+            this.x = this.newX;
+            this.newX = null;
+        }
+
+        ctx.save();
+
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+
+        ctx.restore();
+    }
+
+    clear(ctx: CanvasRenderingContext2D) {
+        ctx.clearRect(this.x, this.y, this.width, this.height);
+    }
+
+    move(x: number, maxWidth: number) {
+        const halfVausWidth = Math.floor(this.width / 2);
+        const newX = x - halfVausWidth;
+
+        if (newX < 0) {
+            this.newX = 0;
+        } else if (x + halfVausWidth > maxWidth) {
+            this.newX = maxWidth - this.width;
+        } else {
+            this.newX = newX;
+        }
+    }
 }
